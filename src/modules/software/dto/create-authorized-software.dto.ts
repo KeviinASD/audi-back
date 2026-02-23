6ad4c2764b848fe2b155e77adee7f9bad12ff955
Kevin <kevinabril093@gmail.com>
2026-02-22 21:06:44 -0500

    refactor: create authorized software dto

--- src/modules/software/dto/create-authorized-software.dto.ts
+++ src/modules/software/dto/create-authorized-software.dto.ts
@@ -1,8 +1,29 @@
 // software/dto/create-authorized-software.dto.ts
 
+import {
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+  MinLength,
+  MaxLength,
+  IsUUID,
+  IsNumber,
+} from 'class-validator';
+
 export class CreateAuthorizedSoftwareDto {
+  @IsNotEmpty({ message: 'El nombre del software es requerido' })
+  @IsString({ message: 'El nombre debe ser un texto' })
   name: string;
+
+  @IsOptional()
+  @IsString({ message: 'El editor debe ser un texto' })
   publisher?: string;
+
+  @IsOptional()
+  @IsString({ message: 'La descripción debe ser un texto' })
   description?: string;
-  laboratoryId?: string;              // null = aplica a todos los laboratorios
+
+  @IsOptional()
+  @IsNumber()
+  laboratoryId?: number; // null = aplica a todos los laboratorios
 }
